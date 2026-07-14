9d8065fd23556a379ac5050a75feb896d03c1b2f
juegosalcaldia <neider.colon@cun.edu.co>
2026-07-13 23:26:21 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -8,5 +8,4 @@
       funcionando con normalidad, pero el registro y el envío de
       correo quedarán deshabilitados en silencio (no rompe el juego).
    ============================================================ */
-
-const APPS_SCRIPT_URL = "PEGA_AQUI_LA_URL_DE_TU_APPS_SCRIPT_WEB_APP";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyg_w1ulAuxIFLhYXQAhJO2avABS482rwaGI3W52Kp-sorCKxkPZyeHLNaT5qR0lkW6Jg/exec";
